9694d034937bcfe06511089a6744713e5c0cd4fe
Minjun Lee <lmjg0407@gmail.com>
2024-07-26 21:29:11 -0500

    feat: added POST request feature. Need to initialize the requestStatus to be pending

--- src/main/java/lawrence/interfaces/RequestController.java
+++ src/main/java/lawrence/interfaces/RequestController.java
@@ -1,4 +1,35 @@
 package lawrence.interfaces;
 
+import lawrence.dtos.RequestDTO;
+import lawrence.securities.CampusSafetyUserDetails;
+import lawrence.services.RequestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/request")
+@CrossOrigin(origins = "*")
 public class RequestController  {
+
+    private RequestService rs;
+
+    public RequestController(RequestService rs) {
+        this.rs = rs;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> save(Authentication authentication, @RequestBody RequestDTO request) {
+        CampusSafetyUserDetails details = (CampusSafetyUserDetails) authentication.getPrincipal();
+        UUID id = UUID.fromString(details.getUsername());
+        request.setRequester(id.toString());
+        String key = rs.save(request);
+        if (key.equals("Bad Id")) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Cannot generate key");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(key);
+    }
 }
--- src/main/java/lawrence/services/RequestService.java
+++ src/main/java/lawrence/services/RequestService.java
@@ -1,5 +1,34 @@
 package lawrence.services;
 
-public class RequestService {
+import lawrence.dtos.RequestDTO;
+import lawrence.entities.Request;
+import lawrence.entities.User;
+import lawrence.repositories.RequestRepository;
+import lawrence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RequestService {
+    @Autowired
+    RequestRepository requestRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String save(RequestDTO request) {
+        Request newRequest = new Request(request);
+        Optional<User> maybeUser = userRepository.findById(UUID.fromString(request.getRequester()));
+        if(!maybeUser.isPresent()) {
+            return "Bad Id";
+        }
+        User user = maybeUser.get();
+        // Not sure if I'm setting the Requester twice. Room for improvement.
+        newRequest.setRequester(user);
+        requestRepository.save(newRequest);
+        return newRequest.getRequestID().toString();
+    }
 }
